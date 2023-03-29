85300eb798f55fc06b768dd03ecbab69a029484a
Annick Savard <annick.savard@gmail.com>
2023-03-29 11:11:49 -0400

    fix: Readd security group module.

--- main.tf
+++ main.tf
@@ -14,21 +14,21 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-/* data "aws_vpc" "default" {
+data "aws_vpc" "default" {
   default = true
 }
- */
+
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  #vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
   }
 }
 
-/* module "blog_sg" {
+module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
@@ -41,4 +41,3 @@ resource "aws_instance" "blog" {
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
- */
