c4250d8fd74e8625c1fbcb92d8d274a42a7eeb19
Annick Savard <annick.savard@gmail.com>
2023-03-29 16:59:53 -0400

    chore: Reorganize modules.

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -36,28 +37,28 @@ module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
-  name = "blog"
+  name     = "blog"
+  min_size = 1
+  max_size = 2
 
-  min_size            = 1
-  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-  instance_type       = var.instance_type
-  image_id            = data.aws_ami.app_ami.id
+
+  instance_type = var.instance_type
+  image_id      = data.aws_ami.app_ami.id
 }
 
+
 module "blog_alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
-
-  name = "blog-alb"
-
+  source             = "terraform-aws-modules/alb/aws"
+  version            = "~> 6.0"
+  name               = "blog-alb"
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
@@ -82,13 +83,14 @@ module "blog_alb" {
   }
 }
 
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
   vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
-  
+
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
