e7cff8f06292d1dc5d6e2a00f7caf9755d35e90c
Annick Savard <annick.savard@gmail.com>
2023-03-29 22:27:22 -0400

    config: Set certif instance to 0.

--- certif/main.tf
+++ certif/main.tf
@@ -6,8 +6,8 @@ module "certif" {
     network_prefix = "10.1"
   }
 
-  asg_min          = 1
-  asg_max          = 1
+  asg_min          = 0
+  asg_max          = 0
   env_name         = "certif"
   env_short_prefix = "cert"
 }
